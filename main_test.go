6d594a49aeb8c9b8204da010de60d35aff3411fa
Shafik Amin <shafik23@gmail.com>
2023-11-03 20:29:23 -0700

    Added input validation tests

--- main_test.go
+++ main_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"math"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -42,4 +45,43 @@ func round(x float64, prec int) float64 {
 
 func closeEnough(a, b, tolerance float64) bool {
 	return math.Abs(a-b) <= tolerance
+}
+
+func TestInputValidation(t *testing.T) {
+	tests := []struct {
+		input          string
+		expectedStatus int
+		expectedBody   string
+	}{
+		{input: "principal=-100&interest_rate=4.5&loan_term_years=30", expectedStatus: http.StatusBadRequest, expectedBody: `{"error":"Invalid principal format - please enter a number in the range [0.1, 10000000]"}`},
+		{input: "principal=200000&interest_rate=-4.5&loan_term_years=30", expectedStatus: http.StatusBadRequest, expectedBody: `{"error":"Invalid interest rate - please enter a number in the range [0.1, 100]"}`},
+		{input: "principal=200000&interest_rate=4.5&loan_term_years=-30", expectedStatus: http.StatusBadRequest, expectedBody: `{"error":"Invalid loan term - please enter a number in the range [1, 100]"}`},
+
+		{input: "principal=200000&interest_rate=4.5&loan_term_years=absdf", expectedStatus: http.StatusBadRequest, expectedBody: `{"error":"Invalid loan term - please enter a number in the range [1, 100]"}`},
+		{input: "principal=200000&interest_rate=dfkjskd&loan_term_years=30", expectedStatus: http.StatusBadRequest, expectedBody: `{"error":"Invalid interest rate - please enter a number in the range [0.1, 100]"}`},
+		{input: "principal=sdkfjsd&interest_rate=4.5&loan_term_years=30", expectedStatus: http.StatusBadRequest, expectedBody: `{"error":"Invalid principal format - please enter a number in the range [0.1, 10000000]"}`},
+	}
+
+	for _, test := range tests {
+		req, err := http.NewRequest("POST", "/calculate", strings.NewReader(test.input))
+		if err != nil {
+			t.Fatal(err)
+		}
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(mortgageHandler)
+
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != test.expectedStatus {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, test.expectedStatus)
+		}
+
+		if rr.Body.String() != test.expectedBody {
+			t.Errorf("handler returned unexpected body: got %v want %v",
+				rr.Body.String(), test.expectedBody)
+		}
+	}
 }

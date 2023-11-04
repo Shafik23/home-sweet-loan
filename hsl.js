752ffc82ece5eb672cc26a7c8a6c2d3616049a1f
Shafik Amin <shafik23@gmail.com>
2023-11-03 23:50:32 -0700

    Added basic history dropdown ... still broken

--- hsl.js
+++ hsl.js
@@ -1,42 +1,63 @@
-document.addEventListener('htmx:afterOnLoad', function (event) {
-  const data = JSON.parse(event.detail.xhr.responseText);
-  if (data.error) {
-    const resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = data.error;
-    resultsDiv.style.color = 'red';
-    resultsDiv.style.fontWeight = 'bold';
-    return;
-  }
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('loan-input').addEventListener('htmx:afterOnLoad', function (event) {
+    const data = JSON.parse(event.detail.xhr.responseText);
+    if (data.error) {
+      const resultsDiv = document.getElementById('results');
+      resultsDiv.innerHTML = data.error;
+      resultsDiv.style.color = 'red';
+      resultsDiv.style.fontWeight = 'bold';
+      return;
+    }
 
-  var table =
-    '<table><tr><th>Month</th><th>Interest</th><th>Principal</th><th>Remaining Balance</th></tr>';
+    var table = '<table><tr><th>Month</th><th>Interest</th><th>Principal</th><th>Remaining Balance</th></tr>';
 
-  data.schedule.forEach(function (monthData) {
-    table +=
-      '<tr><td>' +
-      monthData.month +
-      '</td><td>' +
-      renderNumber(monthData.monthlyInterest) +
-      '</td><td>' +
-      renderNumber(monthData.monthlyPrincipal) +
-      '</td><td>' +
-      renderNumber(monthData.remainingBalance) +
-      '</td></tr>';
+    data.schedule.forEach(function (monthData) {
+      table +=
+        '<tr><td>' +
+        monthData.month +
+        '</td><td>' +
+        renderNumber(monthData.monthlyInterest) +
+        '</td><td>' +
+        renderNumber(monthData.monthlyPrincipal) +
+        '</td><td>' +
+        renderNumber(monthData.remainingBalance) +
+        '</td></tr>';
+    });
+
+    table += '</table>';
+
+    document.getElementById('results').innerHTML =
+      '<h3> Monthly Payment: ' +
+      renderNumber(data.monthlyPayment) +
+      ', Total Payment: ' +
+      renderNumber(data.totalPayment) +
+      ', Total Interest: ' +
+      renderNumber(data.totalInterest) +
+      '<br><br></h3>' +
+      table;
   });
 
-  table += '</table>';
+  const loanHistoryDropdown = document.getElementById('loanHistoryDropdown');
 
-  document.getElementById('results').innerHTML =
-    '<h3> Monthly Payment: ' +
-    renderNumber(data.monthlyPayment) +
-    ', Total Payment: ' +
-    renderNumber(data.totalPayment) +
-    ', Total Interest: ' +
-    renderNumber(data.totalInterest) +
-    '<br><br></h3>' +
-    table;
+  if (loanHistoryDropdown) {
+    loanHistoryDropdown.addEventListener('htmx:afterOnLoad', function (event) {
+      populateLoanHistoryDropdown(JSON.parse(event.detail.xhr.responseText));
+    });
+  }
 });
 
+function populateLoanHistoryDropdown(loans) {
+  const dropdown = document.getElementById('loanHistoryDropdown');
+  let optionsHtml = '<option value="">-- Select a Loan --</option>';
+
+  loans.forEach(function (loan) {
+    optionsHtml += `<option value="${loan.id}">Principal: ${renderNumber(loan.principal)}, Interest Rate: ${
+      loan.interestRate
+    }%, Term: ${loan.loanTerm} years</option>`;
+  });
+
+  dropdown.innerHTML = optionsHtml;
+}
 function renderNumber(number) {
   // Javascript is so quirky! This takes care of the the -0.00 case
   if (number < 0 && number > -0.001) {
